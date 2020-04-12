cc907773d17d5f36260cf18339051089cae94717
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-11 20:51:53 -0400

    [Anmol Singh]Application: Firebase Integrated

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
@@ -3,12 +3,18 @@ package com.lambton.c0777245_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ProgressBar phoneNumberAuthProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        phoneNumberAuthProgressBar = findViewById(R.id.progressBar);
+        phoneNumberAuthProgressBar.setVisibility(View.INVISIBLE);
     }
 }
