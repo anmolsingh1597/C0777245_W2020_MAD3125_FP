2b8573f7f82d38d7d74a3dacc12b24684e205f5e
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-12 14:26:52 -0400

    [Anmol Singh]CustomerList Activity: Generated

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
@@ -106,87 +106,87 @@ public class LoginActivity extends AppCompatActivity {
 //        }
     }
 
+//
+//    private void sendVerificationCode(String mobile) {
+//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+//                mobile,
+//                10,
+//                TimeUnit.SECONDS,
+//                TaskExecutors.MAIN_THREAD,
+//                mCallbacks);
+//    }
+//
+//
+//    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+//        @Override
+//        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+//            //Getting the code sent by SMS
+//            String code = phoneAuthCredential.getSmsCode();
+//            //sometime the code is not detected automatically
+//            //in this case the code will be null
+//            //so user has to manually enter the code
+//            if (code != null) {
+//                mobileNumberAuth.setText(code);
+//                //verifying the code
+//                verifyVerificationCode(code);
+//            }else{
+//                Toast.makeText(LoginActivity.this, "Didn't get the code", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//
+//        @Override
+//        public void onVerificationFailed(FirebaseException e) {
+//            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+//        }
+//
+//        @Override
+//        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+//            super.onCodeSent(s, forceResendingToken);
+//            mVerificationId = s;
+////            mResendToken = forceResendingToken;
+//        }
+//    };
+//    private void verifyVerificationCode(String otp) {
+//        //creating the credential
+//        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, otp);
+//        //signing the user
+//        signInWithPhoneAuthCredential(credential);
+//    }
+//    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+//        mAuth.signInWithCredential(credential)
+//                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            //verification successful we will start the profile activity
+//                            Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
+//                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                            startActivity(intent);
+//
+//                        } else {
+//
+//                            //verification unsuccessful.. display an error message
+//
+//                            String message = "Somthing is wrong, we will fix it soon...";
+//
+//                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+//                                message = "Invalid code entered...";
+//                            }
+//
+//                            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
+//                            snackbar.setAction("Dismiss", new View.OnClickListener() {
+//                                @Override
+//                                public void onClick(View v) {
+//
+//                                }
+//                            });
+//                            snackbar.show();
+//                        }
+//                    }
+//                });
+//    }
 
-    private void sendVerificationCode(String mobile) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                mobile,
-                10,
-                TimeUnit.SECONDS,
-                TaskExecutors.MAIN_THREAD,
-                mCallbacks);
-    }
 
-
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-            //Getting the code sent by SMS
-            String code = phoneAuthCredential.getSmsCode();
-            //sometime the code is not detected automatically
-            //in this case the code will be null
-            //so user has to manually enter the code
-            if (code != null) {
-                mobileNumberAuth.setText(code);
-                //verifying the code
-                verifyVerificationCode(code);
-            }else{
-                Toast.makeText(LoginActivity.this, "Didn't get the code", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-            super.onCodeSent(s, forceResendingToken);
-            mVerificationId = s;
-//            mResendToken = forceResendingToken;
-        }
-    };
-    private void verifyVerificationCode(String otp) {
-        //creating the credential
-        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, otp);
-        //signing the user
-        signInWithPhoneAuthCredential(credential);
-    }
-    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            //verification successful we will start the profile activity
-                            Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(intent);
-
-                        } else {
-
-                            //verification unsuccessful.. display an error message
-
-                            String message = "Somthing is wrong, we will fix it soon...";
-
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                message = "Invalid code entered...";
-                            }
-
-                            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
-                            snackbar.setAction("Dismiss", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-
-                                }
-                            });
-                            snackbar.show();
-                        }
-                    }
-                });
-    }
-
-    
     public void googleSignInButton(View view){
         Toast.makeText(LoginActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
 
