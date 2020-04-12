a7df8d4e2d95d660e56179db987d7ad28838c637
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-12 14:53:27 -0400

    [Anmol Singh]CustomerList Activity: Signed in with Google Successfully

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
@@ -34,16 +34,10 @@ import java.util.concurrent.TimeUnit;
 public class LoginActivity extends AppCompatActivity {
 
     private ProgressBar phoneNumberAuthProgressBar;
-    private TextView mobileNumber;
-    private TextInputLayout mobileNumberAuthLayout;
-    private TextView mobileNumberAuth;
-    private String mVerificationId;
-    private FirebaseAuth mAuth;
+    private TextView status;
     private GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 9001;
     private static final String TAG = "GoogleActivity";
-
-
 
 
     @Override
@@ -57,21 +51,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
+        // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         updateUI(account);
     }
 
     private void initials(){
         phoneNumberAuthProgressBar = findViewById(R.id.progressBar);
-        mobileNumber = findViewById(R.id.enterMobileNumberTextView);
-        mobileNumberAuthLayout = findViewById(R.id.mobileAuthTextView);
-        mobileNumberAuth = findViewById(R.id.enterMobileAuthTextView);
-        mAuth = FirebaseAuth.getInstance();
-
+        status = findViewById(R.id.enterStatusTextView);
+        status.setEnabled(false);
 
         phoneNumberAuthProgressBar.setVisibility(View.INVISIBLE);
-        mobileNumberAuthLayout.setVisibility(View.INVISIBLE);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -82,119 +72,14 @@ public class LoginActivity extends AppCompatActivity {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        // Set the dimensions of the sign-in button.
-        SignInButton signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
     }
 
     public void validateMobileNumber(View view){
-
-        Toast.makeText(LoginActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-
+        phoneNumberAuthProgressBar.setVisibility(View.VISIBLE);
         signIn();
-//
-//        String mobile = "+1" + mobileNumber.getText().toString().trim();
-//        if(mobile.isEmpty() || mobile.length() < 10){
-//            mobileNumber.setError("Enter a valid mobile");
-//            mobileNumber.requestFocus();
-//        }else {
-//            phoneNumberAuthProgressBar.setVisibility(View.VISIBLE);
-//            mobileNumberAuthLayout.setVisibility(View.VISIBLE);
-//            sendVerificationCode(mobile);
-////            Toast.makeText(LoginActivity.this, "Method Called: Send Verification code for " + mobile, Toast.LENGTH_SHORT).show();
-////            verifyVerificationCode(code);
-//        }
     }
 
-//
-//    private void sendVerificationCode(String mobile) {
-//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-//                mobile,
-//                10,
-//                TimeUnit.SECONDS,
-//                TaskExecutors.MAIN_THREAD,
-//                mCallbacks);
-//    }
-//
-//
-//    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//        @Override
-//        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-//            //Getting the code sent by SMS
-//            String code = phoneAuthCredential.getSmsCode();
-//            //sometime the code is not detected automatically
-//            //in this case the code will be null
-//            //so user has to manually enter the code
-//            if (code != null) {
-//                mobileNumberAuth.setText(code);
-//                //verifying the code
-//                verifyVerificationCode(code);
-//            }else{
-//                Toast.makeText(LoginActivity.this, "Didn't get the code", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//
-//        @Override
-//        public void onVerificationFailed(FirebaseException e) {
-//            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-//        }
-//
-//        @Override
-//        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-//            super.onCodeSent(s, forceResendingToken);
-//            mVerificationId = s;
-////            mResendToken = forceResendingToken;
-//        }
-//    };
-//    private void verifyVerificationCode(String otp) {
-//        //creating the credential
-//        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, otp);
-//        //signing the user
-//        signInWithPhoneAuthCredential(credential);
-//    }
-//    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-//        mAuth.signInWithCredential(credential)
-//                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            //verification successful we will start the profile activity
-//                            Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
-//                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                            startActivity(intent);
-//
-//                        } else {
-//
-//                            //verification unsuccessful.. display an error message
-//
-//                            String message = "Somthing is wrong, we will fix it soon...";
-//
-//                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-//                                message = "Invalid code entered...";
-//                            }
-//
-//                            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
-//                            snackbar.setAction("Dismiss", new View.OnClickListener() {
-//                                @Override
-//                                public void onClick(View v) {
-//
-//                                }
-//                            });
-//                            snackbar.show();
-//                        }
-//                    }
-//                });
-//    }
-
-
-    public void googleSignInButton(View view){
-        Toast.makeText(LoginActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
-
-//        signIn();
-
-    }
     public void signIn() {
-//        Toast.makeText(LoginActivity.this, "Sign In fun()", Toast.LENGTH_SHORT).show();
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -228,17 +113,10 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUI(GoogleSignInAccount user) {
         phoneNumberAuthProgressBar.setVisibility(View.INVISIBLE);
         if (user != null) {
-            mobileNumber.setText(getString(R.string.google_status_fmt, user.getEmail()));
-//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-
-//            findViewById(R.id.signInButton).setVisibility(View.GONE);
-//            findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
+            status.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            phoneNumberAuthProgressBar.setVisibility(View.INVISIBLE);
         } else {
-            mobileNumber.setText(R.string.signed_out);
-//            mDetailTextView.setText(null);
-
-//            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
-//            findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
+            status.setText(R.string.signed_out);
         }
     }
 }
