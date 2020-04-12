f0ad53a2b0e997d0e5f70d84e7f40a4679c56a58
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-12 13:28:05 -0400

    [Anmol Singh]Login Activity: Google Sign-In Integrated Successfully

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/LoginActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
@@ -24,7 +31,7 @@ import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ProgressBar phoneNumberAuthProgressBar;
     private TextView mobileNumber;
@@ -32,6 +39,11 @@ public class LoginActivity extends AppCompatActivity {
     private TextView mobileNumberAuth;
     private String mVerificationId;
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "GoogleActivity";
+
+
 
 
     @Override
@@ -39,6 +51,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initials();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
     }
 
     private void initials(){
@@ -51,21 +72,38 @@ public class LoginActivity extends AppCompatActivity {
 
         phoneNumberAuthProgressBar.setVisibility(View.INVISIBLE);
         mobileNumberAuthLayout.setVisibility(View.INVISIBLE);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
     }
 
     public void validateMobileNumber(View view){
 
-        String mobile = "+1" + mobileNumber.getText().toString().trim();
-        if(mobile.isEmpty() || mobile.length() < 10){
-            mobileNumber.setError("Enter a valid mobile");
-            mobileNumber.requestFocus();
-        }else {
-            phoneNumberAuthProgressBar.setVisibility(View.VISIBLE);
-            mobileNumberAuthLayout.setVisibility(View.VISIBLE);
-            sendVerificationCode(mobile);
-//            Toast.makeText(LoginActivity.this, "Method Called: Send Verification code for " + mobile, Toast.LENGTH_SHORT).show();
-//            verifyVerificationCode(code);
-        }
+        Toast.makeText(LoginActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+        signIn();
+//
+//        String mobile = "+1" + mobileNumber.getText().toString().trim();
+//        if(mobile.isEmpty() || mobile.length() < 10){
+//            mobileNumber.setError("Enter a valid mobile");
+//            mobileNumber.requestFocus();
+//        }else {
+//            phoneNumberAuthProgressBar.setVisibility(View.VISIBLE);
+//            mobileNumberAuthLayout.setVisibility(View.VISIBLE);
+//            sendVerificationCode(mobile);
+////            Toast.makeText(LoginActivity.this, "Method Called: Send Verification code for " + mobile, Toast.LENGTH_SHORT).show();
+////            verifyVerificationCode(code);
+//        }
     }
 
 
@@ -146,5 +184,70 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.sign_in_button:
+////                signIn();
+//                break;
+//            // ...
+//        }
+    }
+
+    public void googleSignInButton(View view){
+        Toast.makeText(LoginActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+//        signIn();
+
+    }
+    public void signIn() {
+//        Toast.makeText(LoginActivity.this, "Sign In fun()", Toast.LENGTH_SHORT).show();
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount user) {
+        phoneNumberAuthProgressBar.setVisibility(View.INVISIBLE);
+        if (user != null) {
+            mobileNumber.setText(getString(R.string.google_status_fmt, user.getEmail()));
+//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+//            findViewById(R.id.signInButton).setVisibility(View.GONE);
+//            findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
+        } else {
+            mobileNumber.setText(R.string.signed_out);
+//            mDetailTextView.setText(null);
+
+//            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
+//            findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
+        }
     }
 }
