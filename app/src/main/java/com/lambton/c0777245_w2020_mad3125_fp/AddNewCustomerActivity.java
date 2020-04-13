e50808a90f5baef9b6271f5d187a41fa64fd5028
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-13 17:27:41 -0400

    [Anmol Singh]AddNewCustomerActivity :Layout Designed

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/AddNewCustomerActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/AddNewCustomerActivity.java
@@ -3,6 +3,10 @@ package com.lambton.c0777245_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+//import com.google.firebase.database.core.view.View;
 
 public class AddNewCustomerActivity extends AppCompatActivity {
 
@@ -16,4 +20,11 @@ public class AddNewCustomerActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
     }
+
+  public void saveButtonOnClicked(View view){
+      Toast.makeText(AddNewCustomerActivity.this, "Save", Toast.LENGTH_SHORT).show();
+  }
+  public void clearButtonOnClicked(View view){
+      Toast.makeText(AddNewCustomerActivity.this, "Clear", Toast.LENGTH_SHORT).show();
+  }
 }
