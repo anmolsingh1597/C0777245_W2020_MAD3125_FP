f2561bf66d3f498b5b3f6cd1004fa2e824b365fe
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 17:05:40 -0400

    [Anmol Singh]ShowBillDetailsActivity :Internet Adapter Generated

---
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/adapters/InternetAdapter.java
@@ -0,0 +1,24 @@
+package com.lambton.c0777245_w2020_mad3125_fp.adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class InternetAdapter extends RecyclerView.Adapter<CustomersAdapter.CustomerViewHolder> {
+    @NonNull
+    @Override
+    public CustomersAdapter.CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomersAdapter.CustomerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
