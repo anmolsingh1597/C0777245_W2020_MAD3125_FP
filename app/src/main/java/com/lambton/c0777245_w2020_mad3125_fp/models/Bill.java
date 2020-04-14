0e7819bc43467905a311b1d4a0a3dd8d2ff6cbec
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 16:15:59 -0400

    [Anmol Singh]Bill Class :Variables Declared

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Bill.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Bill.java
@@ -9,4 +9,65 @@ public class Bill implements Serializable {
     String billType;
     String billAmount;
 
+    public Bill(String custId, String id, String date, String billType, String billAmount) {
+        this.custId = custId;
+        this.id = id;
+        this.date = date;
+        this.billType = billType;
+        this.billAmount = billAmount;
+    }
+
+    public Bill() {
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public String getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(String billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "custId='" + custId + '\'' +
+                ", id='" + id + '\'' +
+                ", date='" + date + '\'' +
+                ", billType='" + billType + '\'' +
+                ", billAmount='" + billAmount + '\'' +
+                '}';
+    }
 }
