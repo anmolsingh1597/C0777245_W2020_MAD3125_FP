b2a1ddfcc0b5bde312f36c0c3a434c065614bece
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 17:04:45 -0400

    [Anmol Singh]ShowBillDetailsActivity :Mobile Adapter Generated

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/adapters/MobileAdapter.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/adapters/MobileAdapter.java
@@ -1,4 +1,24 @@
 package com.lambton.c0777245_w2020_mad3125_fp.adapters;
 
-public class MobileAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MobileAdapter extends RecyclerView.Adapter<CustomersAdapter.CustomerViewHolder> {
+    @NonNull
+    @Override
+    public CustomersAdapter.CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomersAdapter.CustomerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
