540f82215306361dc4901aa5ac8e7b55bf2705a5
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 15:46:13 -0400

    [Anmol Singh]ShowBillDetailsActivity : Layout generated

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/ShowBillDetailsActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/ShowBillDetailsActivity.java
@@ -4,31 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lambton.c0777245_w2020_mad3125_fp.models.Customer;
 
 public class ShowBillDetailsActivity extends AppCompatActivity {
 
+    private TextView idTextView;
+    private TextView fullNameTextView;
+    private TextView emailTextView;
+    private TextView mobileTextView;
+
     Bundle customerBundle;
     Customer customerObject;
+//    String id;
+      static String name;
+//    String email;
+//    String mobile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_bill_details);
-
         initials();
     }
 public void initials(){
+        idTextView = findViewById(R.id.billDetailsText1);
+        fullNameTextView = findViewById(R.id.billDetailsText2);
+        emailTextView = findViewById(R.id.billDetailsText3);
+        mobileTextView = findViewById(R.id.billDetailsText4);
     Intent intent = getIntent();
     if (intent.hasExtra("cusObject")){
-       customerBundle = intent.getBundleExtra("cusObject");
-       customerObject = (Customer) customerBundle.getSerializable("customerObject");
-        Toast.makeText(ShowBillDetailsActivity.this, customerObject.fullName(), Toast.LENGTH_SHORT).show();
-    }else {
-        Toast.makeText(ShowBillDetailsActivity.this, "Else", Toast.LENGTH_SHORT).show();
-
+        customerBundle = intent.getBundleExtra("cusObject");
+        customerObject = (Customer) customerBundle.getSerializable("customerObject");
+        name = customerObject.getFirstName();
+        idTextView.setText("Customer Id: " + customerObject.getId());
+        fullNameTextView.setText("Full Name: " + customerObject.fullName());
+        emailTextView.setText("Email: " + customerObject.getEmail());
+        mobileTextView.setText("Mobile: " + customerObject.getMobile());
     }
+    getSupportActionBar().setTitle(name);
 }
+
+    @Override
+    public void onBackPressed() {
+        Intent back = new Intent(ShowBillDetailsActivity.this, CustomerListActivity.class);
+        startActivity(back);
+    }
 }
 
