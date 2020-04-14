3434526fc54f302cb3bf4ea0797cf347964e47a6
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 16:18:27 -0400

    [Anmol Singh]Hydro Class :Variables Declared

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Hydro.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Hydro.java
@@ -1,4 +1,46 @@
 package com.lambton.c0777245_w2020_mad3125_fp.models;
 
 public class Hydro extends Bill {
+    String agencyName;
+    String unitsConsumed;
+
+    public Hydro(String custId, String id, String date, String billType, String billAmount, String agencyName, String unitsConsumed) {
+        super(custId, id, date, billType, billAmount);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    public Hydro(String agencyName, String unitsConsumed) {
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public String getUnitsConsumed() {
+        return unitsConsumed;
+    }
+
+    public void setUnitsConsumed(String unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    @Override
+    public String toString() {
+        return "Hydro{" +
+                "agencyName='" + agencyName + '\'' +
+                ", unitsConsumed='" + unitsConsumed + '\'' +
+                ", custId='" + custId + '\'' +
+                ", id='" + id + '\'' +
+                ", date='" + date + '\'' +
+                ", billType='" + billType + '\'' +
+                ", billAmount='" + billAmount + '\'' +
+                '}';
+    }
 }
