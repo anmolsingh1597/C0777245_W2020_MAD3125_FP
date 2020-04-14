63af094ed467dc7ebf8ff4c55867fb77a654b9f6
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 16:20:42 -0400

    [Anmol Singh]Internet Class :Constructor Declared

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Internet.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Internet.java
@@ -4,4 +4,15 @@ public class Internet extends Bill {
     String internetGb;
     String providerName;
 
+    public Internet(String custId, String id, String date, String billType, String billAmount, String internetGb, String providerName) {
+        super(custId, id, date, billType, billAmount);
+        this.internetGb = internetGb;
+        this.providerName = providerName;
+    }
+
+    public Internet(String internetGb, String providerName) {
+        this.internetGb = internetGb;
+        this.providerName = providerName;
+    }
+
 }
