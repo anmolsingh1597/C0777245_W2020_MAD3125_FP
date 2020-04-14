a2832c792ad68ae4b37c46629ef6efe5a5630df9
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 16:23:32 -0400

    [Anmol Singh]Mobile Class :Variables Generated

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Mobile.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/models/Mobile.java
@@ -1,4 +1,10 @@
 package com.lambton.c0777245_w2020_mad3125_fp.models;
 
-public class Mobile {
+public class Mobile extends Bill {
+    String mobileManufacturer;
+    String planName;
+    String mobileNumber;
+    String internetGb;
+    String minutes;
+
 }
