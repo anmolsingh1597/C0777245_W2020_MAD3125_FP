7761ed7d208398e290aaf1c95b658e307dd4ee52
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-16 16:54:48 -0400

    [Anmol Singh]AddNewBillActivity :Layout arranged dynamicallly

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/AddNewBillActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/AddNewBillActivity.java
@@ -7,7 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.DateFormat;
 import java.text.DateFormatSymbols;
@@ -20,6 +25,24 @@ public class AddNewBillActivity extends AppCompatActivity {
     private TextView customerId;
     private TextView billId;
     private TextView date;
+    private RadioGroup billTypeRadioGroup;
+//    private RadioButton mobileRadioButton;
+//    private RadioButton internetRadioButton;
+//    private RadioButton hydroRadioButton;
+    private RadioButton billTypeRadioButton;
+    private TextView generalTextView1;
+    private TextView generalTextView2;
+    private TextView generalTextView3;
+    private TextView generalTextView4;
+    private TextView generalTextView5;
+    private TextInputLayout generalTView1;
+    private TextInputLayout generalTView2;
+    private TextInputLayout generalTView3;
+    private TextInputLayout generalTView4;
+    private TextInputLayout generalTView5;
+    private TextView totalBillAmountTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +56,33 @@ public class AddNewBillActivity extends AppCompatActivity {
         customerId = findViewById(R.id.entercustIdTextView);
         billId = findViewById(R.id.enterBillIdTextView);
         date = findViewById(R.id.enterDateTextView);
+        billTypeRadioGroup = findViewById(R.id.billTypeRadioGroup);
+//        mobileRadioButton = findViewById(R.id.mobileRadioButton);
+//        internetRadioButton = findViewById(R.id.internetRadioButton);
+//        hydroRadioButton = findViewById(R.id.hydroRadioButton);
+        generalTextView1 = findViewById(R.id.enterGeneralTextView1);
+        generalTextView2 = findViewById(R.id.enterGeneralTextView2);
+        generalTextView3 = findViewById(R.id.enterGeneralTextView3);
+        generalTextView4 = findViewById(R.id.enterGeneralTextView4);
+        generalTextView5 = findViewById(R.id.enterGeneralTextView5);
+        generalTView1 = findViewById(R.id.generalTextView1);
+        generalTView2 = findViewById(R.id.generalTextView2);
+        generalTView3 = findViewById(R.id.generalTextView3);
+        generalTView4 = findViewById(R.id.generalTextView4);
+        generalTView5 = findViewById(R.id.generalTextView5);
+
+
+        totalBillAmountTextView = findViewById(R.id.enterTotalBillTextView);
+
         Intent intent = getIntent();
         if(intent.hasExtra("custId")){
             customerId.setText(intent.getStringExtra("custId"));
             customerId.setEnabled(false);
         }
         date.setEnabled(false);
+
+
+
     }
 
     public void dateImageButton(View view){
@@ -71,5 +115,40 @@ public class AddNewBillActivity extends AppCompatActivity {
             month = months[num];
         }
         return month;
+    }
+
+    public void radioGroupAction(){
+        int selectedId = billTypeRadioGroup.getCheckedRadioButtonId();
+            billTypeRadioButton = findViewById(selectedId);
+            if(billTypeRadioButton.getText().toString().equals("Mobile")){
+                generalTView1.setHint("Mobile Manufacturer");
+                generalTView2.setHint("Mobile Number");
+                generalTView3.setHint("Plan Name");
+                generalTView4.setHint("Internet GB used");
+                generalTView5.setHint("Minutes Consumed");
+                generalTView3.setVisibility(View.VISIBLE);
+                generalTView4.setVisibility(View.VISIBLE);
+                generalTView5.setVisibility(View.VISIBLE);
+            }else if(billTypeRadioButton.getText().toString().equals("Internet")){
+                generalTView1.setHint("Provider Name");
+                generalTView2.setHint("Internet GB Used");
+                generalTView3.setVisibility(View.GONE);
+                generalTView4.setVisibility(View.GONE);
+                generalTView5.setVisibility(View.GONE);
+
+            }else if(billTypeRadioButton.getText().toString().equals("Hydro")){
+                generalTView1.setHint("Agency Name");
+                generalTView2.setHint("Units Consumed");
+                generalTView3.setVisibility(View.GONE);
+                generalTView4.setVisibility(View.GONE);
+                generalTView5.setVisibility(View.GONE);
+            }
+
+
+        
+    }
+
+    public void onClickBillSaveButton(View view){
+        radioGroupAction();
     }
 }
