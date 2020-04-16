faa32a09fa9600866e8dc4437ce56ca8a96af0f0
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-15 23:31:01 -0400

    [Anmol Singh]AddNewBillActivity : Layout Designed

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/AddNewBillActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/AddNewBillActivity.java
@@ -10,5 +10,6 @@ public class AddNewBillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_bill);
+        getSupportActionBar().hide();
     }
 }
