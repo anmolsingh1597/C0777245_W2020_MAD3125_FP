236a91bf38035e5bcc1d5110263dfbf6b790985b
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-19 21:30:34 -0400

    [Anmol Singh]AddNewCustomerActivity : Email Validation Integrated

--- app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/interfaces/AddNewCustomerActivity.java
+++ app/src/main/java/com/lambton/c0777245_w2020_mad3125_fp/interfaces/AddNewCustomerActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,6 +13,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.lambton.c0777245_w2020_mad3125_fp.R;
 import com.lambton.c0777245_w2020_mad3125_fp.models.Customer;
+
+import java.util.regex.Pattern;
 
 
 public class AddNewCustomerActivity extends AppCompatActivity {
@@ -58,9 +62,9 @@ public class AddNewCustomerActivity extends AppCompatActivity {
                 firstNameText.setError("Enter First Name");
           }else if(lastName.equals("")){
               lastNameText.setError("Enter Last Name");
-          }else if(email.equals("")){
+          }else if(!isValidEmailId(email.trim())){
               emailText.setError("Invalid Email");
-          }else if(mobile.equals("") || mobile.length()<10){
+          }else if(mobile.equals("") && mobile.length()<10){
               mobileText.setError("Invalid Mobile Number");
           }
           else {
@@ -82,4 +86,14 @@ public class AddNewCustomerActivity extends AppCompatActivity {
         customerObject = new Customer("CUS_"+id,firstName,lastName,email,mobile);
         myRef.push().setValue(customerObject);
   }
+
+    private boolean isValidEmailId(String email){
+
+        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
+    }
 }
